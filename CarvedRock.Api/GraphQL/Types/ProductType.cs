ca0725534849baf8c47e3d7f7e8dc508d01a6661
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-07-03 06:38:03 +0200

    Added ProductReview

--- CarvedRock.Api/GraphQL/Types/ProductType.cs
+++ CarvedRock.Api/GraphQL/Types/ProductType.cs
@@ -1,11 +1,13 @@
 ﻿using CarvedRock.Api.Data.Entities;
+using CarvedRock.Api.Repositories;
+using GraphQL.DataLoader;
 using GraphQL.Types;
 
 namespace CarvedRock.Api.GraphQL.Types
 {
     public class ProductType: ObjectGraphType<Product>
     {
-        public ProductType()
+        public ProductType(ProductReviewRepository reviewRepository, IDataLoaderContextAccessor dataLoaderAccessor)
         {
             Field(t => t.Id);
             Field(t => t.Name).Description("The name of the product");
@@ -16,6 +18,15 @@ namespace CarvedRock.Api.GraphQL.Types
             Field(t => t.Rating).Description("The (max 5) star customer rating");
             Field(t => t.Stock);
             Field<ProductTypeEnumType>("Type", "The type of product");
+            Field<ListGraphType<ProductReviewType>>(
+                "reviews",
+                resolve: context =>
+                {
+                    var loader =
+                        dataLoaderAccessor.Context.GetOrAddCollectionBatchLoader<int, ProductReview>(
+                            "GetReviewsByProductId", reviewRepository.GetForProducts);
+                    return loader.LoadAsync(context.Source.Id);
+                });
 
         }
     }
