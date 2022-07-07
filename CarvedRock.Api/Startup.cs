f5ed09430b9d2a2290e424a0338a9eeb07cfb681
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-07-07 07:42:06 +0200

    Added the message service.

--- CarvedRock.Api/Startup.cs
+++ CarvedRock.Api/Startup.cs
@@ -33,6 +33,7 @@ namespace CarvedRock.Api
 
             services.AddScoped<IDependencyResolver>(s => new FuncDependencyResolver(s.GetRequiredService));
             services.AddScoped<CarvedRockSchema>();
+            services.AddSingleton<ReviewMessageService>();
 
             services.AddGraphQL(o => { o.ExposeExceptions = _env.IsDevelopment(); })
                 .AddGraphTypes(ServiceLifetime.Scoped)
